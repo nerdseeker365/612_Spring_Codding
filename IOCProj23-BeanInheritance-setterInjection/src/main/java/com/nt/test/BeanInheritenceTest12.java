81dee1bf517b263798b60e802d1a05c8de1316ac
sankar <karrasankar09@gmail.com>
2020-04-05 17:52:36 +0530

    BeanInheritance ,common properties separate abstract="true" and specific properties separate parent="common properties bean id"

--- IOCProj23-BeanInheritance-setterInjection/src/main/java/com/nt/test/BeanInheritenceTest12.java
+++ IOCProj23-BeanInheritance-setterInjection/src/main/java/com/nt/test/BeanInheritenceTest12.java
@@ -1,29 +1,30 @@
-package com.nt.test;  
+package com.nt.test;
 
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 
 import com.nt.beans.Car;
 
-public class BeanInheritenceTest {
+public class BeanInheritenceTest12 {
 
 	public static void main(String[] args) {
-		DefaultListableBeanFactory factory=null;
-		XmlBeanDefinitionReader reader=null;
-		 Car car=null,car1=null;
-		//Create IOC Container
-		factory=new DefaultListableBeanFactory();
-		reader=new XmlBeanDefinitionReader(factory);
+		DefaultListableBeanFactory factory = null;
+		XmlBeanDefinitionReader reader = null;
+		Car car = null, car1 = null;
+		// Create IOC Container
+		factory = new DefaultListableBeanFactory();
+		reader = new XmlBeanDefinitionReader(factory);
 		reader.loadBeanDefinitions("com/nt/cfgs/applicationContext.xml");
-		//get Spring bean class objects
-		/*abstract="true" cannot create object,thats why comment....
+		// get Spring bean class objects
+		/*
+		 * abstract="true" cannot create object,thats why comment....
 		 * car1=factory.getBean("baseCar",Car.class); System.out.println(car1);
 		 * System.out.println(".....................................");
 		 */
-		car=factory.getBean("rajaCar", Car.class);
+		car = factory.getBean("rajaCar", Car.class);
 		System.out.println(car);
 		System.out.println("------------------------------------------------");
-		car1=factory.getBean("raniCar", Car.class);
+		car1 = factory.getBean("raniCar", Car.class);
 		System.out.println(car1);
 		System.out.println("------------------------------------------------");
 	}
